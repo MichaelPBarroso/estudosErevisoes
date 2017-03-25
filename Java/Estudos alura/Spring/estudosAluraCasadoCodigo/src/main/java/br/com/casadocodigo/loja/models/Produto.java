9c8f4567ddc8a43411bb5ac3583d2c1e2c001e41
MichaelPBarroso <michael_p._b.15600@hotmail.com>
2017-03-25 12:32:17 -0300

    Atualização Spring - Complementando cadastro do produto

--- Java/Estudos alura/Spring/estudosAluraCasadoCodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ Java/Estudos alura/Spring/estudosAluraCasadoCodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,7 +17,10 @@ public class Produto {
 	
 	private String titulo;
 	private String descricao;
-	private String paginas;
+	private int paginas;
+	
+	@ElementCollection
+	private List<Preco> precos;
 	
 	public String getTitulo() {
 		return titulo;
@@ -28,15 +34,26 @@ public class Produto {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	public String getPaginas() {
+	public int getPaginas() {
 		return paginas;
 	}
-	public void setPaginas(String paginas) {
+	public void setPaginas(int paginas) {
 		this.paginas = paginas;
 	}
-	
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 }
